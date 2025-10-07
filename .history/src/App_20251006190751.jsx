67e357b6ec744386d7b5a6660cfd5682e8410cca
bbrockbrown <brockbrown46@gmail.com>
2025-10-06 19:13:23 -0500

    initial commit

---
+++ .history/src/App_20251006190751.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return(
+    <main>
+      <h1>TODO LIST</h1>
+    </main>
+  );
+}
+
+export default App
---
+++ .history/src/App_20251006191159.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+function App() {
+  return(
+    <main>
+      <h1>TODO LIST</h1>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: 5, backgroundColor: 'lightgray' }}>
+        <div className='task'>
+          <h4>Washing laundry</h4>
+          <p>Wash</p>
+          <span>tomorrow</span>
+        </div>
+        <div className='task'>
+          <h4>Washing laundry</h4>
+          <p>Wash</p>
+          <span>tomorrow</span>
+        </div>
+        <div className='task'>
+          <h4>Washing laundry</h4>
+          <p>Wash</p>
+          <span>tomorrow</span>
+        </div>
+        <div className='task'>
+          <h4>Washing laundry</h4>
+          <p>Wash</p>
+          <span>tomorrow</span>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App
